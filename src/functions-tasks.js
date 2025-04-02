28581f83aa5337b1e41c05d13d8582c789515407
Anna Vasilevich <dzichonka@gmail.com>
2025-04-02 19:17:28 +0200

    feat: passed 11/11 tests

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -239,8 +239,12 @@ function partialUsingArguments(fn, ...args1) {
  *   getId4() => 7
  *   getId10() => 11
  */
-function getIdGeneratorFunction(/* startFrom */) {
-  throw new Error('Not implemented');
+function getIdGeneratorFunction(startFrom) {
+  let startId = startFrom - 1;
+  return () => {
+    startId += 1;
+    return startId;
+  };
 }
 
 module.exports = {
